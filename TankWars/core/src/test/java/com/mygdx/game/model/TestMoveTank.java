3d48d8673db29548d56f2459c8cb448f387929d9
Leyla Arale <leylaa@student.chalmers.se>
2018-04-24 11:34:38 +0200

    Ändrar även i TestMoveTank

--- TankWars/core/src/test/java/com/mygdx/game/model/TestMoveTank.java
+++ TankWars/core/src/test/java/com/mygdx/game/model/TestMoveTank.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertEquals;
 public class TestMoveTank {
 
     @Test
-    public void testMove(){
+    public void testMoveRight(){
         //test1 - checks change in x-coordinate
         Tank tank = new Tank(new Position(30, 0), 50, 50, 90);
-        int expectedPos = 31;
-        tank.moveTank();
+        int expectedPos= 31;
+        tank.moveTankRight();
         int result = tank.positionTank.x;
 
         assertEquals(expectedPos, result);
