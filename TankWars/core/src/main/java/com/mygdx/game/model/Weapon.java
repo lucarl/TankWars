3481b0d6799de02fac7e2883e5e5eb45a54e5e4e
Leyla Arale <leylaa@student.chalmers.se>
2018-04-24 09:19:46 +0200

    Merge branch 'local' of /Users/marianarale/Desktop/TDA367/TankWars with conflicts.

--- TankWars/core/src/main/java/com/mygdx/game/model/Weapon.java
+++ TankWars/core/src/main/java/com/mygdx/game/model/Weapon.java
@@ -1,9 +1,13 @@
 package com.mygdx.game.model;
 
+import java.util.Vector;
+
+
 /**
  * Created by Carl on 2018-04-19.
  */
 public class Weapon {
-    int ammo;
+    volatile int ammo;
+    Vector vector;
 
 }
