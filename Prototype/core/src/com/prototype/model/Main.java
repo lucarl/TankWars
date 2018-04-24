44cf21fed17895d5dc314f03af8bf311bf697a45
lucarl <carl.e.w.lundborg@gmail.com>
2018-04-24 09:25:51 +0200

    added class Shot

---
+++ Prototype/core/src/com/prototype/model/Main.java
@@ -0,0 +1,28 @@
+public class Model {
+    private Galaxy galaxy;
+    private Player player;
+    private boolean active = false;
+
+    /**
+     * Start a new game.
+     */
+    public void startNewGame(Controller controller) { // TODO: Clean up magic screens
+        this.galaxy = GalaxyFactory.get().make();
+        this.player = new Player(new ObjectIntMap<>(), galaxy.getStations().get("Homeworld"), galaxy.getStations().get("Homeworld"));
+        this.player.getQuests().add(new QuestFactory().make("INTRO_QUEST", controller, this));
+        this.active = true;
+    }
+
+    public void dispose() { }
+
+    // === Getters / Setters === //
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Galaxy getGalaxy() { return galaxy; }
+
+    public boolean isActive() {
+        return active;
+    }
+}
---
+++ TankWars/core/src/main/java/com/mygdx/game/model/Shot.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.model;
+
+/**
+ * Created by Carl on 2018-04-23.
+ */
+public class Shot {
+
+
+}
