08af76f64829f50d472aa321a1e592fae6fdcb0b
adamk <adam.kjall@hotmail.com>
2018-04-25 15:39:54 +0200

    Update shot class

--- TankWars/core/src/main/java/com/mygdx/game/model/Shot.java
+++ TankWars/core/src/main/java/com/mygdx/game/model/Shot.java
@@ -4,6 +4,24 @@ package com.mygdx.game.model;
  * Created by Carl on 2018-04-23.
  */
 public class Shot {
+    private int[] vector;
+    private int x, y;
+    private int damage;
+    private float radius;
+    private float weight;
+
+
+
+    public Shot(int x, int y, int[] vector, int damage, int radius, int weight){
+        this.x = x;
+        this.y = y;
+        this.vector = vector;
+        this.damage = damage;
+        this.radius = radius;
+        this.weight = weight;
+    }
+
+    
 
 
 }
