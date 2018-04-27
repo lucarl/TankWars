cb3bfaf52adc4317a84aed8450cde464c7a3ee6a
adamk <adam.kjall@hotmail.com>
2018-04-27 13:13:39 +0200

    Set foreground fps = 60

--- TankWars/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ TankWars/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,21 +3,15 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.ctrl.Controller;
-import com.mygdx.game.model.Demo;
 import com.mygdx.game.model.TankWars;
-import com.mygdx.game.view.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		TankWars tankWars = new TankWars();
 		Controller controller = new Controller(tankWars);
-		//PlayScreen baseScreen = new PlayScreen(controller, tankWars);
 		
-
-
-
-
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
 		config.width = 1000;
 		config.height = 600;
 		new LwjglApplication(controller, config);
