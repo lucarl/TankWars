c16ae7c50dee3268d5b22c5e91b29864df7f5b4c
lucarl <carl.e.w.lundborg@gmail.com>
2018-05-06 15:29:44 +0200

    test coordinates

--- TankWars/core/src/main/java/com/mygdx/game/model/Terrain.java
+++ TankWars/core/src/main/java/com/mygdx/game/model/Terrain.java
@@ -3,12 +3,18 @@ package com.mygdx.game.model;
 public class Terrain {
     private TerrainTile[][] terrainMatrix;
     private static final String imageSource = "terrain.png";
+    int yPositionOffset;
+    int xPositionOffset;
 
     public Terrain() {
-        this.terrainMatrix = new TerrainTile[1000][200];
+        this.yPositionOffset = 200;
+        this.terrainMatrix = new TerrainTile[200][40];
         for (int i = 0; i < terrainMatrix.length; i++) {
+            xPositionOffset = -5;
+            yPositionOffset -= 5;
             for (int j = 0; j < terrainMatrix[i].length; j++) {
-                terrainMatrix[i][j] = new TerrainTile(i * 5, j * 5, true);
+                xPositionOffset += 5;
+                terrainMatrix[i][j] = new TerrainTile(yPositionOffset, xPositionOffset, true);
             }
         }
     }
--- TankWars/core/src/main/java/com/mygdx/game/model/TerrainTile.java
+++ TankWars/core/src/main/java/com/mygdx/game/model/TerrainTile.java
@@ -2,11 +2,11 @@ package com.mygdx.game.model;
 import java.util.Random;
 
 public class TerrainTile {
-    private float x, y;
+    private int x, y;
     private int width, height = 5;
     private boolean alive;
 
-    public TerrainTile(int x, int y, boolean alive) {
+    public TerrainTile(int y, int x, boolean alive) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -30,19 +30,8 @@ public class TerrainTile {
         return height;
     }
 
-
-
-
-
-
-    /*public Terrain() {
-        tileMap = new Object[1000][250];
-        for (int i = 0; i < tileMap.length; i++) {
-            for (int j = 0; j < tileMap[i].length; j++) {
-                tileMap[i][j] = new Object
-            }
-        }
-
-    }*/
+    public boolean isAlive() {
+        return alive;
+    }
 
 }
