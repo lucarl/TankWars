bf6ec0e695680f6f223ffd54130556776b742eb0
lucarl <carl.e.w.lundborg@gmail.com>
2018-05-07 13:04:31 +0200

    implementing factory

---
+++ TankWars/core/src/test/java/com/mygdx/game/model/testTerrain.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.model;
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class testTerrain {
+
+    @Test
+    public void testCoordinates() {
+        Terrain terrain = new Terrain();
+        TerrainTile[][] terrainTiles = terrain.getTerrainMatrix();
+        assertTrue(terrainTiles[0][0].getPos().getX() == 0);
+        assertTrue(terrainTiles[0][0].getPos().getY() == 195);
+        assertTrue(terrainTiles[0][1].getPos().getX() == 5);
+        assertTrue(terrainTiles[2][0].getPos().getY() == 185);
+        assertTrue(terrainTiles[2][2].getPos().getX() == 10);
+        assertTrue(terrainTiles[2][2].isAlive());
+    }
+}
